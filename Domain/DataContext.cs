6185b10883009bf07f6d9df46f4e88b37d139dfc
Manuel Antonio Batres Luna <al155192@edu.uaa.mx>
2019-12-15 20:42:38 -0600

    League

---
+++ Domain/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<Domain.League> Leagues { get; set; }
+    }
+}
